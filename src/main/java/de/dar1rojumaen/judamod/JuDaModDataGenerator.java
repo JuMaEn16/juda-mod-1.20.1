0205c6cf26dfb22bc6b07f3b6abbbfd4278395a7
JuMaEn <jumaen401@gmail.com>
2024-11-27 16:51:44 +0100

    Initial commit

---
+++ src/main/java/de/dar1rojumaen/judamod/JuDaModDataGenerator.java
@@ -0,0 +1,11 @@
+package de.dar1rojumaen.judamod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JuDaModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
